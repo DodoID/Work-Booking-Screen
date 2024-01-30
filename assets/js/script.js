9215dcf25ec7287b69b6363e26be401ffe339412
Lordominus <daniel.idiaconu@gmail.com>
2024-01-30 18:03:20 +0000

    JS updated working day hours

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,13 @@
 $(document).ready(function() {
     // Extend dayjs with the advanced format plugin
     dayjs.extend(window.dayjs_plugin_advancedFormat);
+
+     // Define working day hours as an array of strings
+     const workingDayHours = [
+        "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"
+      ];
+    
+      // Display the current date
+      const currentDateTime = dayjs();
+      $("#currentDay").text(currentDateTime.format("[Today is:] dddd[,] MMMM Do"));
+      
