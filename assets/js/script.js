9bfaf1bdf6e49f890aded3e1ccea8226b4d88280
Lordominus <daniel.idiaconu@gmail.com>
2024-01-30 18:11:00 +0000

    JS added further code for working hours, local storage, updated HTML & CSS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,22 @@
 $(document).ready(function() {
-    // Extend dayjs with the advanced format plugin
+    // Extended dayjs with the advanced format plugin
     dayjs.extend(window.dayjs_plugin_advancedFormat);
 
-     // Define working day hours as an array of strings
+     // Defined working day hours as an array of strings
      const workingDayHours = [
         "9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"
       ];
     
-      // Display the current date
+      // To display the current date
       const currentDateTime = dayjs();
       $("#currentDay").text(currentDateTime.format("[Today is:] dddd[,] MMMM Do"));
-      
+      // To display the current hour
+    const currentHour = currentDateTime.format("[Current hour:] HH:mm");
+    $("header").append("<p class='currentHour'>" + currentHour + "</p>");
+    
+  
+    // Created time blocks for each working hour
+    function createHourBlocks(hours) {
+    
+    // For retrieving user tasks from local storage or initialize an empty array
+      const tasksList = JSON.parse(localStorage.getItem("userTasks")) || [];
